3f09e3d69c796b1a185f5f108ba95b39d32429f3
66rhythm <tanyabh2k1@gmail.com>
2021-02-11 02:43:29 +0530

    c

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:Project/screens/product_details.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import "./screens/product_overview_screen.dart";
+import './providers/products.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,15 +11,20 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        accentColor: Colors.red[400],
-        fontFamily: 'Lato',
-      ),
-      home: ProductOverviewScreen(),
-    );
+    return ChangeNotifierProvider(
+        create: (ctx) => Products(),
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            accentColor: Colors.red[400],
+            fontFamily: 'Lato',
+          ),
+          home: ProductOverviewScreen(),
+          routes: {
+            ProductDetails.routeName: (ctx) => ProductDetails(),
+          },
+        ));
   }
 }
 //I am making changes
