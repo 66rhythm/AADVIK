1059b44c1113b8fc12c477444af4fe7ba5fcd0bd
66rhythm <tanyabh2k1@gmail.com>
2021-02-19 23:25:51 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:Project/screens/product_details.dart';
 import 'package:Project/screens/user_product_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
-import "./screens/product_overview_screen.dart";
+// import "./screens/product_overview_screen.dart";
 import './providers/products.dart';
 import './providers/cart.dart';
 import './screens/cart_screen.dart';
 import 'providers/orders.dart';
 import './screens/orders_screen.dart';
 import './screens/edit_product_screen.dart';
+import './screens/auth_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -36,7 +37,7 @@ class MyApp extends StatelessWidget {
             accentColor: Colors.red[400],
             fontFamily: 'Lato',
           ),
-          home: ProductOverviewScreen(),
+          home: AuthScreen(),
           routes: {
             ProductDetails.routeName: (ctx) => ProductDetails(),
             CartScreen.roueName: (ctx) => CartScreen(),
