4569f6eaab5824fdbf56d37aa097f5645c0477fb
66rhythm <tanyabh2k1@gmail.com>
2021-02-22 23:10:51 +0530

    logout widget aded

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../screens/orders_screen.dart';
 import '../screens/user_product_screen.dart';
+import '../providers/auth.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
@@ -44,6 +46,17 @@ class AppDrawer extends StatelessWidget {
           onTap: () {
             Navigator.of(context)
                 .pushReplacementNamed(UserProductsScreen.routeName);
+          },
+        ),
+        Divider(),
+        ListTile(
+          leading: Icon(Icons.exit_to_app),
+          title: Text('Log Out'),
+          onTap: () {
+            Navigator.of(context).pop();
+            // Navigator.of(context)
+            //     .pushReplacementNamed(UserProductsScreen.routeName);
+            Provider.of<Auth>(context, listen: false).logout();
           },
         ),
       ],
